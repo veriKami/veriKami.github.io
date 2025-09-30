e5ab3fac044b61bcf8ba3385ced2ce2d1aed21be
Kami Weronika Amelia <verikami@gmail.com>
2025-09-30 08:21:03 +0200

    +: google site verification

--- scripts/make.html.js
+++ scripts/make.html.js
@@ -28,6 +28,7 @@ const makeHtml = ($ = {}) => {
     <head>
     <meta charset="UTF-8">
     <title>veriKami.github.io</title>
+    <meta name="google-site-verification" content="qGe3Iz5C890mQVRy9dgEO5r5uALAa1kY_w-0GtLsAd8">
     <style>
     body { font-family: sans-serif; font-size: 1rem; padding: .2rem 3rem 1rem; background: #fff; }
     hr { margin: 0 0 1rem; height: 1px; border-width: 0; background-color: #ccc; }
