8bd09373733d3787ffbde327442c3090c799b0be
Kami Weronika Amelia <verikami@gmail.com>
2025-10-01 19:12:49 +0200

    october's update

--- scripts/make.html.js
+++ scripts/make.html.js
@@ -4,10 +4,10 @@
 //: --------------------------------------------------------
 import fs from "node:fs";
 import path from "node:path";
+import { execSync } from "node:child_process";
 import dedent from "dedent";
 import { remark } from "remark";
 import remarkHtml from "remark-html";
-import { execSync } from "node:child_process";
 //: --------------------------------------------------------
 import deflist from "@verikami/remark-deflist-revisited";
 
@@ -27,43 +27,45 @@ const makeHtml = ($ = {}) => {
     <html lang="en">
     <head>
     <meta charset="UTF-8">
-    <title>veriKami °// Weronika Kami °// veriKami.github.io</title>
+    <title>veriKami °// Weronika Kami °// Chasing Technoscience</title>
     <meta name="description" content="">
     <meta name="keywords" content="remark, remark-plugin, remark-deflist, markdown, markdown-plugin,
       definition-list, nested-lists, deflist, unist, astro, parser, wrapper, veriKami, Weronika Kami">
     <meta name="author" content="veriKami °// Weronika Kami">
     <meta name="pubdate" content="2025/09/30">
+    <meta name="robots" content="index, follow">
     <meta name="google-site-verification" content="qGe3Iz5C890mQVRy9dgEO5r5uALAa1kY_w-0GtLsAd8">
     <style>
-    body { font-family: sans-serif; font-size: 1rem; padding: .2rem 3rem 1rem; background: #fff; }
-    hr { margin: 0 0 1rem; height: 1px; border-width: 0; background-color: #ccc; }
-    pre, dl { border: 1px solid #ccc; margin: 1rem 0; padding: 1rem; background: #fff; }
-    pre { background: #f8f8f8; font-size: .85rem; }
-    dt { font-weight: bold; margin-bottom: .5rem; }
-    dd { margin: 0 1rem .5rem; color: gray; }
-    dd ul, dd ol { margin-left: 0; color: darkblue; }
-    ul, ol { margin-left: 2rem; color: red; }
-    ul li ul, ol li ol { margin-left: 0; }
-    table { border-collapse: collapse; background: #fff; }
-    tr:nth-child(2) td:first-child { color: #aaa; font-weight: normal; }
-    tr:nth-child(2) a { color: darkTurquoise; }
-    td { padding: .3rem .5rem; border: 1px solid #ddd; }
-    td:first-child { text-align: right; font-weight: bold; }
-    a, a:visited { color: blue; text-decoration: none; }
-    a:hover { text-decoration: underline; }
-    h1 a, h1 a:hover { text-decoration: none; }
-    /* *********************************** */
-    navigate li { margin-left: 2rem; padding: .5rem; color: red; }
-    navigate li p,
-    navigate li code { color: black; }
-    navigate li code { padding: .3rem .5rem; background: #f5f5f5; border:1px solid #ddd; }
-    /* *********************************** */
+      body { font-family: sans-serif; font-size: 1rem; padding: .2rem 3rem 1rem; background: #fff; }
+      hr { margin: 0 0 1rem; height: 1px; border-width: 0; background-color: #ccc; }
+      pre, dl { border: 1px solid #ccc; margin: 1rem 0; padding: 1rem; background: #fff; }
+      pre { background: #f8f8f8; font-size: .85rem; }
+      dt { font-weight: bold; margin-bottom: .5rem; }
+      dd { margin: 0 1rem .5rem; color: gray; }
+      dd ul, dd ol { margin-left: 0; color: darkblue; }
+      ul, ol { margin-left: 2rem; color: red; }
+      ul li ul, ol li ol { margin-left: 0; }
+      table { border-collapse: collapse; background: #fff; }
+      tr:nth-child(2) td:first-child { color: #aaa; font-weight: normal; }
+      tr:nth-child(2) a { color: darkTurquoise; }
+      td { padding: .3rem .5rem; border: 1px solid #ddd; }
+      td:first-child { text-align: right; font-weight: bold; }
+      a, a:visited { color: blue; text-decoration: none; }
+      a:hover { text-decoration: underline; }
+      h1 a, h1 a:hover { text-decoration: none; }
+      navigate li { margin-left: 2rem; padding: .5rem; color: red; }
+      navigate li p,
+      navigate li code { color: black; }
+      navigate li code { padding: .3rem .5rem; background: #f5f5f5; border:1px solid #ddd; }
+      hr { height: 1px; margin: 15px 0 15px; }
+      details { margin: 0; padding: 0; }
+      summary { margin: 0 0 30px; padding: 0; }
     </style>
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-3TQT133E82"></script>
     <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag() { dataLayer.push(arguments); }
-    gtag("js", new Date()); gtag("config", "G-3TQT133E82");
+      window.dataLayer = window.dataLayer || [];
+      function gtag() { dataLayer.push(arguments); }
+      gtag("js", new Date()); gtag("config", "G-3TQT133E82");
     </script>
     </head>
     <body>
@@ -125,12 +127,14 @@ const htmlHeader = `
 const makeFiles = (mode) => {
   mode = mode ? mode + "." : "";
 
+  //: ---------------------------------------
   files.forEach(file => {
     try {
       const input = fs.readFileSync(path.join(sourceDir, file), "utf8");
+
       const html = remark()
         .use(deflist)
-        .use(remarkHtml)
+        .use(remarkHtml, { sanitize: false })
         .processSync(input)
         .toString()
         // .replace("@verikami/remark-deflist-revisited", "<hr>");
@@ -150,7 +154,8 @@ const makeFiles = (mode) => {
 
       fs.writeFileSync(outputPath, htmlPage, "utf8");
       console.log(`Generated: ${outputFileName}`);
-    } catch (err) {
+    }
+    catch (err) {
       console.error(`Error processing file ${file}:`, err.message);
     }
   });
